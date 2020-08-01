9bdd0930ca226868b4c0f093e015bbf5f86a950e
RodwelJunior09 <rodwelpolancomartinez@gmail.com>
2020-08-01 18:40:27 -0400

    Splash Screen, Loading Screen, Level Design

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        var sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (sceneIndex == 0)
+        {
+            StartCoroutine(LoadStartMenu());
+        }
+    }
+
+    public IEnumerator LoadStartMenu()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void LoadNextScene()
+    {
+        int currentIndexScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndexScene + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
