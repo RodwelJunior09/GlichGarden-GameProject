6dd9938fe1e35ffb82575c8c5de70569c38cd82f
Rodwel Polanco Martinez <rodwelpolancomartinez@gmail.com>
2020-10-18 22:55:34 -0400

    Fixing health system, spawing defenders, Twicking cactus defender

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            DisplayVFX();
+            Destroy(gameObject);
+        }
+    }
+
+    private void DisplayVFX()
+    {
+        if (!deathVFX) { return; }
+        Instantiate(deathVFX, transform.position, transform.rotation);
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,6 +3,9 @@
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject _projectile;
+    [SerializeField] float minSpawnDelay = 0f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
     public void Fire()
     {
         Instantiate(_projectile, transform.position, transform.rotation);
