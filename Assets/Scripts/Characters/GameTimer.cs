c4d7c1eb01ca0c530348a5a2c1c0a3bdb936e03a
Rodwel Polanco Martinez <rodwelpolancomartinez@gmail.com>
2020-11-10 00:12:29 -0400

    Making Level controller script

---
+++ Assets/Scripts/Characters/GameTimer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer in seconds")]
+    [SerializeField] float levelTime = 10;
+    bool timerFinished;
+
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+    }
+
+    public bool TimerFinished() => timerFinished;
+}
---
+++ Assets/Scripts/UI/LevelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public int GetNumberAttackersOnScreen()
+    {
+        return FindObjectsOfType<Attacker>().Length;
+    }
+
+    public bool TimerFinished()
+    {
+        return FindObjectOfType<GameTimer>().TimerFinished();
+    }
+
+    public void StopSpawning()
+    {
+        FindObjectOfType<Spawner>().StopSpawn();
+    }
+
+    public void PlayerWin()
+    {
+        FindObjectOfType<SceneLoader>().LoadNextScene();
+    }
+}
